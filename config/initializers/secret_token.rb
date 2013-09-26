deeeb439bacee1f3db4f314e2777e2f7d635fd03
Diligasi <diligasi@gmail.com>
2013-09-26 01:36:35 -0300

    Base files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rhodnius::Application.config.secret_key_base = '0cead8c569fc708c5568d2801111d58267f55c4de86c35d10967e2b900327767af631b428e91794824a63b0690e407fad0e7a2c1ba386488c28a26788c9a3732'
