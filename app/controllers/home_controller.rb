21ac02ba35e275bbdd5d6f28cff940c76391634a
Diogo <diligasi@gmail.com>
2014-10-01 03:44:58 -0300

    Fixed bug of null when no camera on db.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     session[:id_camera] = params[:selected_cam_id] unless params[:selected_cam_id].blank?
     @current_camera = session[:id_camera] ? Camera.find(session[:id_camera]) : nil
     # todo: Montar o link com as câmeras por ip
-    @current_cam_adress = @current_camera.full_url.blank? ? 'soon...' : @current_camera.full_url
+    @current_cam_adress =  @current_camera && !@current_camera.full_url.blank? ? @current_camera.full_url : nil
     @comment = Comment.new
   end
 end
